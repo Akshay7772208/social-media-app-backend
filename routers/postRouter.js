6c95815daaff39219b2deee13a5dfea77e8d779d
Akshay7772208 <134836865+Akshay7772208@users.noreply.github.com>
2023-06-02 19:12:08 +0530

    Update postRouter.js

--- routers/postRouter.js
+++ routers/postRouter.js
@@ -5,7 +5,7 @@ const requireUser=require('../middlewares/requireUser')
 const UserController=require('../controllers/userController')
 
 //router.get('/all',requireUser,postsController.getAllPostsController)
-router.post('/login',authController.loginController)
+//router.post('/login',authController.loginController)
 //router.post("/",requireUser,postsController.createPostController)
 router.post('/like',requireUser,postsController.likeAndUnlikePost)
 router.put('/',requireUser,postsController.updatePostController)
